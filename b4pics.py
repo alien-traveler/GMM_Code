81fa4ac78f7dec8d496530eca5b40b4ed907b118
alien-traveler <jiwenwillluo@gmail.com>
2020-11-08 14:13:05 -0500

    cut all graphs into small pieces 11.8.20

--- b4pics.py
+++ b4pics.py
@@ -30,27 +30,47 @@ def check_size():
         image = Image.open(os.path.join(path, filename))
         print(image.size)
 
-# crop the image into the size we need
-def crop_original_img():
-    count = 0
-    xi, yi, xf, yf = 235, 685, 1636, 1595
-
+# check the folder dir
+def check_folder_dir():
     path = 'new_test_pics/new_test_pics2/'
-    img = Image.open(os.path.join(path, "Gel_C9.jpg"))
+    folder_name_list = []
+    for filename in os.listdir(path):
+        folder_name_list.append(filename)
+    print(folder_name_list)
+
+
+
+# crop the image into the size we need
+def crop_original_img(name, x1, y1):
+    count = 0
+
+    # x1, y1是首坐标，在函数被调用前修改，函数内部不能修改
+    
+
+    #xi, xf, yi, yf是每一张小图的斜对角坐标, 函数内move on切图时会改变
+    x2 = x1 + 1400
+    xi, xf = x1, x2
+    yi = y1        # y的首坐标
+    yf = yi + 910   # y的初始坐标
+    imgname = name + ".jpg"
+    path = 'new_test_pics/new_test_pics2/'
+    img = Image.open(os.path.join(path, imgname))
     for i in range(3):
         for j in range(3):
-            box = (xi, yi, xf, yf) # this is the starting point, and the image moves on by increasing (1410, 550) every time
+            box = (xi, yi, xf, yf) # this is the starting point, and the image moves on by increasing (1400, 910) every time
             new_img = img.crop(box)
-            new_img.save(path + str(count) + '.jpg')
+            new_img.save(path + name + '/' + str(count) + '.jpg')
             count += 1
             xi += 1430
             xf += 1430
-        xi = 235
-        xf = 1635
+        xi = x1
+        xf = x2
         yi += 930
         yf += 930
 
 
-
 if __name__ == "__main__":
-    crop_original_img()
+    """check_folder_dir()"""
+    name = "Gel_C5"
+    x, y = 920, 430    # 这是首坐标，第一张要切的小图在大图中的（左上角）坐标
+    crop_original_img(name, x, y)
